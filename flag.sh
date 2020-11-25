47c8e3e5f4c0887496b662f4f83cb7d7e039b587
Jesus E. Dominguez <57687264+jesusdominguezb@users.noreply.github.com>
2020-11-25 12:25:59 -0600

    Create flag.sh

---
+++ flag.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for i in  {1..100}
+do
+
+    if [ "$i" = "50" ]; then
+
+        toilet -f mono12 -F metal --filter border:metal "Aquí    está la bandera    |       v"
+        echo "flag:       69e5be8a77f17604de9bb06aaab6d3b548f97ced"
+
+    else
+        echo "Seguridad e Integridad de la Información"
+    fi
+done
